2dfff1b9e0ebf6765ecc28c60fb84c81353dfd38
laiiihz <laiiihz@outlook.com>
2020-09-16 15:59:00 +0800

    首屏时间期望减少一半

--- lib/pages/welcome/launch_widget.dart
+++ lib/pages/welcome/launch_widget.dart
@@ -14,35 +14,34 @@ import 'package:recook/constants/constants.dart';
 import 'package:recook/utils/app_router.dart';
 import 'package:sprintf/sprintf.dart';
 
-
 class LaunchWidget extends StatefulWidget {
   @override
   _LaunchWidgetState createState() => _LaunchWidgetState();
 }
 
-class _LaunchWidgetState extends BaseStoreState<LaunchWidget> with SingleTickerProviderStateMixin {
-
-  Map<int,Image> _imageMap = {}; 
+class _LaunchWidgetState extends BaseStoreState<LaunchWidget>
+    with SingleTickerProviderStateMixin {
+  Map<int, Image> _imageMap = {};
   // GifController _gifController;
 
   @override
   void dispose() {
-
     super.dispose();
   }
 
   @override
   void initState() {
     // _gifController = GifController(vsync: this);
-    
+
     super.initState();
     //0-59
     for (var i = 0; i <= 104; i++) {
-      _imageMap.putIfAbsent(i, (){
-        return Image.asset("assets/recook_launch_image/recook_launch_image_$i.png");
+      _imageMap.putIfAbsent(i, () {
+        return Image.asset(
+            "assets/recook_launch_image/recook_launch_image_$i.png");
       });
     }
-    WidgetsBinding.instance.addPostFrameCallback((callback){
+    WidgetsBinding.instance.addPostFrameCallback((callback) {
       // _gifController.repeat(min: 0, max:59, period: Duration(milliseconds: 1500));
       Future.delayed(Duration(milliseconds: 3500), () {
         AppRouter.fadeAndReplaced(globalContext, RouteName.WELCOME_PAGE);
@@ -55,24 +54,29 @@ class _LaunchWidgetState extends BaseStoreState<LaunchWidget> with SingleTickerP
     Constants.initial(context);
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    
+
     return Scaffold(
       body: Container(
-        child: ImagesAnimation(w: width, h: height, milliseconds: 3500, entry: ImagesAnimationEntry(0, 104, "assets/recook_launch_image/recook_launch_image_%s.png")),
+        child: ImagesAnimation(
+            w: width,
+            h: height,
+            milliseconds: 1750,
+            entry: ImagesAnimationEntry(0, 104,
+                "assets/recook_launch_image/recook_launch_image_%s.png")),
       ),
     );
   }
 }
 
-
 class ImagesAnimation extends StatefulWidget {
-
   final double w;
   final double h;
   final ImagesAnimationEntry entry;
   final int milliseconds;
-  
-  ImagesAnimation({Key key, this.w : 80, this.h : 80, this.entry, this.milliseconds : 1000}):super(key:key);
+
+  ImagesAnimation(
+      {Key key, this.w: 80, this.h: 80, this.entry, this.milliseconds: 1000})
+      : super(key: key);
 
   @override
   _InState createState() {
@@ -80,19 +84,21 @@ class ImagesAnimation extends StatefulWidget {
   }
 }
 
-class _InState extends State<ImagesAnimation> with TickerProviderStateMixin{
-
+class _InState extends State<ImagesAnimation> with TickerProviderStateMixin {
   AnimationController _controller;
   Animation<int> _animation;
 
   @override
   void initState() {
     super.initState();
-    _controller = new AnimationController(vsync: this, duration: Duration(milliseconds: widget.milliseconds));
-    _animation = new IntTween(begin: widget.entry.lowIndex, end: widget.entry.highIndex).animate(_controller)
-      ..addListener((){
-        setState(() {});
-      });
+    _controller = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: widget.milliseconds));
+    _animation =
+        new IntTween(begin: widget.entry.lowIndex, end: widget.entry.highIndex)
+            .animate(_controller)
+              ..addListener(() {
+                setState(() {});
+              });
     _controller.forward();
 //widget.entry.lowIndex 表示从第几下标开始，如0；widget.entry.highIndex表示最大下标：如7
   }
@@ -105,7 +111,6 @@ class _InState extends State<ImagesAnimation> with TickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
-
     return new AnimatedBuilder(
       animation: _animation,
       builder: (BuildContext context, Widget child) {
@@ -119,7 +124,6 @@ class _InState extends State<ImagesAnimation> with TickerProviderStateMixin{
       },
     );
   }
-
 }
 
 class ImagesAnimationEntry {
