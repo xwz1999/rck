641040e5512a4e8540afd735f6566a2c025c1a85
laiiihz <laiiihz@outlook.com>
2020-09-17 22:15:15 +0800

    release-1.2.0+161

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ void main() async {
     // useEncrypt: true
   );
   // 设置当前是否为测试环境
-  bool isDebug = true;
+  bool isDebug = false;
   AppConfig.setDebug(isDebug);
 
   bool inTest = Test.test();
